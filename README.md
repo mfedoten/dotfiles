4139b06118a3a45ba0cb673ca78c3a5b54f25bba
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2015-08-01 17:44:44 +0200

    Add initial install script and its description to README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dotfiles
+
+## Installation
+
+1. Git
+Obviously, we will need [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+
+2. Terminal
+I prefer iTerm2 over default OSX Terminal.app. The script `install.sh` installs iTerm into `/Applications/Utilities` folder if it is not there yet.
+
+## Other programs I have installed
+* [Google Chrome](https://www.google.com/chrome/browser/desktop/)
--- install.sh
+++ install.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/env bash
 
-# Install MacPorts
-# from https://guide.macports.org/#installing.shell
-curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.3.tar.bz2
-tar xf MacPorts-2.3.3.tar.bz2
-cd MacPorts-2.3.3/
-./configure
-make
-sudo make install
-cd ..
-sudo rm -r MacPorts-2.3.3
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+# Ask for the administrator password upfront
+sudo -v
+
+# # Install MacPorts
+# # from https://guide.macports.org/#installing.shell
+# curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.3.3.tar.bz2
+# tar xf MacPorts-2.3.3.tar.bz2
+# cd MacPorts-2.3.3/
+# ./configure
+# make
+# sudo make install
+# cd ..
+# sudo rm -r MacPorts-2.3.3
+# export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
+# Install iTerm2
+echo 'Installing iTerm into /Applications/Utilities'
+if [[ -d /Applications/Utilities/iTerm.app ]]; then
+    echo 'iTerm is already installed in /Applications/Utilities'
+else
+    curl -O https://iterm2.com/downloads/stable/iTerm2-2_1_1.zip
+    unzip -q iTerm2-2_1_1.zip
+    rm iTerm2-2_1_1.zip
+    sudo mv iTerm.app /Applications/Utilities
+    if [[ -d /Applications/Utilities/iTerm.app ]]; then
+        echo 'iTerm is installed in /Applications/Utilities'
+    else
+        echo 'Something went wrong. Cleaning up'
+        rm -r iTerm2-2_1_1.zip ./iTerm.app
+    fi
+fi
 
