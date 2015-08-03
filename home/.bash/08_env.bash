196199d086420605280a2a087b43faa805cff965
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2015-08-03 19:18:23 +0200

    add colors to less

--- home/.bash/08_env.bash
+++ home/.bash/08_env.bash
@@ -10,6 +10,16 @@ export LC_ALL='en_US.UTF-8';
 # Set less as default pagerm highlight section titles in manual pages.
 export PAGER=less
 export LESS=-R
+man() {
+    env LESS_TERMCAP_mb=$'\E[01;31m' \
+    LESS_TERMCAP_md=$'\E[01;38;5;74m' \
+    LESS_TERMCAP_me=$'\E[0m' \
+    LESS_TERMCAP_se=$'\E[0m' \
+    LESS_TERMCAP_so=$'\E[38;5;246m' \
+    LESS_TERMCAP_ue=$'\E[0m' \
+    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    man "$@"
+}
 
 # Autocorrect typos in path names when using `cd`
 shopt -s cdspell
