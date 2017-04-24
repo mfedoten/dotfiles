ede6d0974f945bad651f60583c640d31c3438412
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-04-24 18:13:27 +0200

    BASH: tree func alias

--- home/.bash/aliases.bash
+++ home/.bash/aliases.bash
@@ -76,7 +76,7 @@ fi
 alias week='date +%V'
 
 # Tree: show all files and hide git dir
-alias tree='tree -a -I ".git"'
+alias gtree='tree -a -I ".git"'
 
 # Git updates under curent dir
 # alias upgit='find . -maxdepth 2 -mindepth 1 -name .git -type d -prune | while read d; do cd $d/..; gecho -e "${Cyan}$PWD ${Color_Off} git pull"; git pull; cd $OLDPWD; done'
