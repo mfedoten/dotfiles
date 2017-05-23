d82692f0055e233f9963f3f3c03b995fc8537bfe
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-05-23 12:08:43 +0200

    PY: start matplotlib in ipython on startup

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -1,5 +1,5 @@
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2', '%matplotlib']
 
 c.TerminalInteractiveShell.highlighting_style = 'monokai'
 c.InteractiveShell.colors = 'neutral'
