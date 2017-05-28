67f44f206ba2309ab43f9a5c1f22c70483ae08b7
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-05-28 13:55:58 +0200

    OSX: change ports to homebrew

---
+++ init/10_osx_homebrew.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+# Ask for the administrator password upfront
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Exit the script if there's any error
+set -e
+
+# Logging stuff.
+function e_header()   { echo -e "\n\033[1m$@\033[0m"; }
+function e_success()  { echo -e " \033[1;32m✔\033[0m  $@"; }
+function e_error()    { echo -e " \033[1;31m✖\033[0m  $@"; }
+function e_arrow()    { echo -e " \033[1;34m➜\033[0m  $@"; }
+
+# Given strings containing space-delimited words A and B, "setdiff A B" will
+# return all words in A that do not exist in B. Arrays in bash are insane
+# (and not in a good way).
+# From http://stackoverflow.com/a/1617303/142339
+function setdiff() {
+  local debug skip a b
+  if [[ "$1" == 1 ]]; then debug=1; shift; fi
+  if [[ "$1" ]]; then
+    local setdiff_new setdiff_cur setdiff_out
+    setdiff_new=($1); setdiff_cur=($2)
+  fi
+  setdiff_out=()
+  for a in "${setdiff_new[@]}"; do
+    skip=
+    for b in "${setdiff_cur[@]}"; do
+      [[ "$a" == "$b" ]] && skip=1 && break
+    done
+    [[ "$skip" ]] || setdiff_out=("${setdiff_out[@]}" "$a")
+  done
+  [[ "$debug" ]] && for a in setdiff_new setdiff_cur setdiff_out; do
+    echo "$a ($(eval echo "\${#$a[*]}")) $(eval echo "\${$a[*]}")" 1>&2
+  done
+  [[ "$1" ]] && echo "${setdiff_out[@]}"
+}
+
+# Install Homebrew.
+if [[ ! "$(type -P brew)" ]]; then
+  e_header "Installing Homebrew"
+  true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Exit if, for some reason, Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Homebrew failed to install." && return 1
+
+e_header "Updating Homebrew"
+brew doctor
+brew update
+
+# Functions used in subsequent init scripts.
+
+# Tap Homebrew kegs.
+function brew_tap_kegs() {
+  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
+  if (( ${#kegs[@]} > 0 )); then
+    e_header "Tapping Homebrew kegs: ${kegs[*]}"
+    for keg in "${kegs[@]}"; do
+      brew tap $keg
+    done
+  fi
+}
+
+# Install Homebrew recipes.
+function brew_install_recipes() {
+  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
+  if (( ${#recipes[@]} > 0 )); then
+    e_header "Installing Homebrew recipes: ${recipes[*]}"
+    for recipe in "${recipes[@]}"; do
+      brew install $recipe
+    done
+  fi
+}
