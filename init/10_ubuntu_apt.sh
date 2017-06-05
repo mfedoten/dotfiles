0ad6ad8d3cea0e89e7952f4f7d09a89bb63cf6aa
mfedoten <maria.fedotenkova@gmail.com>
2017-06-05 02:47:46 +0200

    modifyed installation script for apt packages

--- init/10_ubuntu_apt.sh
+++ init/10_ubuntu_apt.sh
@@ -17,42 +17,37 @@ sudo apt-get -qq update
 sudo apt-get -qq dist-upgrade
 
 # Read ports into array
-mapfile -t packages < apt-packages.txt
+packages=(
+  coreutils
+  findutils
+  build-essential
+  psutils
+  htop
+  curl
+  xclip
+  imagemagick
+  cowsay
+  fortunes
+  sl
+  tree
+  screenfetch
+  bash-completion
+  dconf-editor
+  compizconfig-settings-manager
+  ruby-full
+  tmux
+  checkinstall
+  tilda
+  exuberant-ctags
+  smbclient
+  samba
+)
 
-# Given strings containing space-delimited words A and B, "setdiff A B" will
-# return all words in A that do not exist in B. Arrays in bash are insane
-# (and not in a good way).
-# From http://stackoverflow.com/a/1617303/142339
-function setdiff() {
-  local debug skip a b
-  if [[ "$1" == 1 ]]; then debug=1; shift; fi
-  if [[ "$1" ]]; then
-    local setdiffA setdiffB setdiffC
-    setdiffA=($1); setdiffB=($2)
-  fi
-  setdiffC=()
-  for a in "${setdiffA[@]}"; do
-    skip=
-    for b in "${setdiffB[@]}"; do
-      [[ "$a" == "$b" ]] && skip=1 && break
-    done
-    [[ "$skip" ]] || setdiffC=("${setdiffC[@]}" "$a")
-  done
-  [[ "$debug" ]] && for a in setdiffA setdiffB setdiffC; do
-    echo "$a ($(eval echo "\${#$a[*]}")) $(eval echo "\${$a[*]}")" 1>&2
-  done
-  [[ "$1" ]] && echo "${setdiffC[@]}"
-}
 
-# The rest is taken from https://github.com/cowboy/dotfiles
-packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
-
-if (( ${#packages[@]} > 0 )); then
-  echo -e "\n\033[1mInstalling APT packages: ${packages[*]}\033[0m"
-  for package in "${packages[@]}"; do
-    sudo apt-get -qq install "$package"
-  done
-fi
+echo -e "\n\033[1mInstalling APT packages: ${packages[*]}\033[0m"
+for package in "${packages[@]}"; do
+  sudo apt-get -qq install "$package"
+done
 
 # get the latest git
 sudo apt-add-repository ppa:git-core/ppa
