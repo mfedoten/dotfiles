6fb7d0bd4f19b0f75e833dfa71445afcce45aedd
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-06-22 15:47:22 +0200

    PY: bullshit!

--- home/.ipython/profile_default/ipython_config.py
+++ home/.ipython/profile_default/ipython_config.py
@@ -1,5 +1,5 @@
 c.InteractiveShellApp.extensions = ['autoreload']
-c.InteractiveShellApp.exec_lines = ['%autoreload 2', '%matplotlib']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 
 c.InteractiveShellApp.matplotlib = 'qt5'
 
