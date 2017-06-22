6cca71a9b79db1d44f92d6fb5d353b0456d694d4
Mariia Fedotenkova <maria.fedotenkova@gmail.com>
2017-06-22 14:58:26 +0200

    PY: bullshit!

--- home/.ipython/profile_default/ipython_kernel_config.py
+++ home/.ipython/profile_default/ipython_kernel_config.py
@@ -1,3 +1,3 @@
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['import sys', 'import os', 'import numpy as np','%autoreload 2']
-c.InteractiveShellApp.matplotlib = 'qt5'
+#c.InteractiveShellApp.matplotlib = 'qt5'
